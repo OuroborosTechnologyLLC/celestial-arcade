a59fb4e569fa2bfab8771727f56e04230a7d9af9
OuroborosTechnologyLLC <zackrisongeorge@disroot.org>
2025-10-24 00:50:41 -0500

    PhaserJS; simple road demo

--- plate-run/main.js
+++ plate-run/main.js
@@ -1,7 +1,21 @@
-let modulePath = './scene.js';
-let sceneModule = await import(`${modulePath}?t=${Date.now()}`);
+import * as Phaser from './libs/phaser.esm.js';
 
-sceneModule.init(); // your scene setup + start loop
+let modulePath = './scenes/mainScene.js';
+const { default: MainScene } = await import(`${modulePath}?t=${Date.now()}`);
+
+const config = {
+	type: Phaser.AUTO,
+	backgroundColor: 'rgb(100, 200, 50)',
+	scale: {
+		mode: Phaser.Scale.RESIZE, // auto-resize with window
+		autoCenter: Phaser.Scale.CENTER_BOTH,
+		width: '100%',
+		height: '100%',
+	},
+	scene: [ MainScene ],
+};
+const game = new Phaser.Game(config);
+window.game = game;
 
 // Optional: auto-reload every few seconds for dev (polling method)
 // setInterval(async () => {
